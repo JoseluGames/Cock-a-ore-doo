933f247bbb16d319054218a221b9f8c19dca8ff0
JoseluGames <joselugamesofficial@gmail.com>
2016-09-02 18:09:19 +0200

    Fill mcmod.info and change recipes

--- src/main/java/com/jlgm/caod/crafting/CAODCrafting.java
+++ src/main/java/com/jlgm/caod/crafting/CAODCrafting.java
@@ -8,11 +8,11 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CAODCrafting {
 	public static void registerCrafting(){
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.COAL), new Object[]{CAODItem.coalEgg, CAODItem.coalEgg});
-		GameRegistry.addRecipe(new ItemStack(Items.IRON_INGOT), new Object[]{"###", "# #", "###", '#', CAODItem.ironEgg});
-		GameRegistry.addRecipe(new ItemStack(Items.GOLD_INGOT), new Object[]{"###", "# #", "###", '#', CAODItem.goldEgg});
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.DYE, 1, 4), new Object[]{CAODItem.lapisEgg, CAODItem.lapisEgg});
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.REDSTONE), new Object[]{CAODItem.redstoneEgg, CAODItem.redstoneEgg});
-		GameRegistry.addRecipe(new ItemStack(Items.DIAMOND), new Object[]{"###", "###", "###", '#', CAODItem.diamondEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.COAL, 2), new Object[]{"##", "##", '#', CAODItem.coalEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.IRON_INGOT), new Object[]{"##", "##", '#', CAODItem.ironEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.GOLD_INGOT), new Object[]{"##", "##", '#', CAODItem.goldEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.DYE, 1, 4), new Object[]{"##", "##", '#', CAODItem.lapisEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.REDSTONE, 4), new Object[]{"##", "##", '#', CAODItem.redstoneEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.DIAMOND), new Object[]{"##", "##", '#', CAODItem.diamondEgg});
 	}
 }
