e3454263e0b1f3469f24630e2dfed9c8d51c2133
JoseluGames <joselugamesofficial@gmail.com>
2016-09-02 19:38:59 +0200

    Fix crafting recipes

--- src/main/java/com/jlgm/caod/crafting/CAODCrafting.java
+++ src/main/java/com/jlgm/caod/crafting/CAODCrafting.java
@@ -11,9 +11,10 @@ public class CAODCrafting {
 		GameRegistry.addRecipe(new ItemStack(Items.COAL, 2), new Object[]{"##", "##", '#', CAODItem.coalEgg});
 		GameRegistry.addRecipe(new ItemStack(Items.IRON_INGOT), new Object[]{"##", "##", '#', CAODItem.ironEgg});
 		GameRegistry.addRecipe(new ItemStack(Items.GOLD_INGOT), new Object[]{"##", "##", '#', CAODItem.goldEgg});
-		GameRegistry.addRecipe(new ItemStack(Items.DYE, 1, 4), new Object[]{"##", "##", '#', CAODItem.lapisEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.DYE, 4, 4), new Object[]{"##", "##", '#', CAODItem.lapisEgg});
 		GameRegistry.addRecipe(new ItemStack(Items.REDSTONE, 4), new Object[]{"##", "##", '#', CAODItem.redstoneEgg});
-		GameRegistry.addRecipe(new ItemStack(Items.GLOWSTONE_DUST, 4), new Object[]{"##", "##", '#', CAODItem.redstoneEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.GLOWSTONE_DUST, 4), new Object[]{"##", "##", '#', CAODItem.glowstoneEgg});
+		GameRegistry.addRecipe(new ItemStack(Items.EMERALD), new Object[]{"##", "##", '#', CAODItem.emeraldEgg});
 		GameRegistry.addRecipe(new ItemStack(Items.DIAMOND), new Object[]{"##", "##", '#', CAODItem.diamondEgg});
 	}
 }
